d89c7bbb591df6a253d95d824a171f4fbb0ba2a1
Anton B <booran@yandex.ru>
2024-12-27 16:44:30 +0900

    after merge

--- HelloGit.py
+++ HelloGit.py
@@ -1,6 +1,7 @@
+import os
 import b
-print('Hello, Git!')
-print('Indexing is important!')
+print('Hello, Git')
+print('Indexing - it is important')
 
 ind = 0
 sum = 0
@@ -10,4 +11,8 @@ while ind <= 9:
         print(sum)
         ind += 1
 
-print(b.any_funk(2,sum))
+print(b.any_func(2, sum))
+print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
