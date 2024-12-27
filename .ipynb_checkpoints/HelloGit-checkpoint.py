07da7236848af52c32ad6755209daded8097a667
Anton B <booran@yandex.ru>
2024-12-27 16:14:45 +0900

    Update hello_git py. Add for

---
+++ .ipynb_checkpoints/HelloGit-checkpoint.py
@@ -0,0 +1,3 @@
+print("Hello Git!")
+print('Индексация - это важно')
+print('Create new branch')
--- HelloGit.py
+++ HelloGit.py
@@ -1,3 +1,5 @@
 print("Hello Git!")
 print('Индексация - это важно')
 print('Create new branch')
+for num in range(10):
+        print(num,end=' ')
